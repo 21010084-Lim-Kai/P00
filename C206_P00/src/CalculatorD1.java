35c54991a50265e47010ad749267c184833e3d4a
Kai <Kai@172.29.79.117>
2022-05-30 15:11:24 +0800

    First commit to P00

---
+++ C206_P00/src/CalculatorD1.java
@@ -0,0 +1,34 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Lim Kai, 5.00 p.m.
+ */
+
+/**
+ * @author Kai
+ *
+ */
+public class CalculatorD1 {
+	
+	public int add(int a, int b)
+	{
+	return a + b;
+	}
+	
+	public int sub(int a, int b) 
+	{
+	return a - b;
+	}
+	
+	public int mul(int a, int b) 
+	{
+	return a * b;
+	}
+	
+	public int div(int a, int b) 
+	{
+	return a / b;
+	}
+}
---
+++ C206_P00/test/CalculatorD1Test.java
@@ -0,0 +1,80 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//test class
+public class CalculatorD1Test {
+int a,b; 
+CalculatorD1 cal;
+
+
+	@Before
+	public void setUp() throws Exception {
+		a = 4321;
+		b = 1234;
+		cal = new CalculatorD1();
+		
+	}
+	
+	@Test
+	public void testadd() {
+
+		CalculatorD1 cal = new CalculatorD1();
+		
+		//Act 
+		int actual = cal.add(a,b);
+		int expected = 5555;
+		
+		//Assertion
+		assertEquals(expected, actual);
+	}
+	
+	
+	@Test
+	public void testsub() {
+		//Arrange
+
+		CalculatorD1 cal = new CalculatorD1();
+		
+		//Act 
+		int actual = cal.sub(a,b);
+		int expected = 3087;
+		
+		//Assertion
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testsmul() {
+		//Arrange
+		CalculatorD1 cal = new CalculatorD1();
+		
+		//Act 
+		int actual = cal.mul(a,b);
+		int expected = 5332114;
+		
+		//Assertion
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testdiv() {
+		//Arrange
+		CalculatorD1 cal = new CalculatorD1();
+		
+		//Act 
+		int actual = cal.div(a,b);
+		int expected = 3;
+		
+		//Assertion
+		assertEquals(expected, actual);
+	}
+	
+		
+	@After
+	public void tearDown() throws Exception {
+	}
+	}
+
